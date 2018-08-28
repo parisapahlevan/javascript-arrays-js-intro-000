c3477380478c99519681d0baa9bf9ab0c111547d
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-28 17:37:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ];
+function addElementToBeginningOfArray(array, elements){
+ ["jnj", ...chocolateBars] 
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+ chocolateBars.unshift(hgjg)  
+}
